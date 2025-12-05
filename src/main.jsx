a556994ad90e8278ee02c82626b9d75df166228c
sajet4819 <sajetdhopare72@gmail.com>
2025-12-05 23:32:42 +0530

    Fix basename and image paths for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,14 @@ import { Provider } from "react-redux";
 import App from "./App.jsx";
 import { store } from "./redux/store.js"; // Correctly import the store we just created
 import "./index.css";
+const basename = import.meta.env.MODE === 'production' ? '/top-Tutions' : '/'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* Provide the Redux store to the entire application */}
     <Provider store={store}>
       {/* Provide routing capabilities to the entire application */}
-      <BrowserRouter basename="/top-Tutions">
+      <BrowserRouter basename={basename}>
   <App />
 </BrowserRouter>
     </Provider>
